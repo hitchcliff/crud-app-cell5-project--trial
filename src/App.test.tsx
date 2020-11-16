d8efd83be66ecadee09b8caf41f9f8ac23742ccd
kevin nacario <hitchcliff1@gmail.com>
2020-11-17 07:28:44 +0800

    initialize

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+
+import { configure, shallow, ShallowWrapper } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+configure({ adapter: new Adapter() });
+
+/**
+ * Function that return shallow `component`
+ * @returns {ShallowWrapper}
+ */
+const setup = () => {
+  const wrapper = shallow(<App />);
+  return wrapper;
+};
+
+describe('App component tests', () => {
+  let wrapper: ShallowWrapper;
+
+  beforeEach(() => {
+    wrapper = setup();
+  });
+
+  test('should render without error', () => {
+    expect(wrapper.length).toBe(1);
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { useSelector, useDispatch } from 'react-redux';
-import { TestActions } from './Actions/test.action';
-import { RootStore } from './Store';
+import React from 'react';
 
-function App() {
-  const state = useSelector((state: RootStore) => state.test);
-  const { loading, testTodos } = state;
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(TestActions());
-  }, []);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is a boilerplate for{' '}
-          <code>React, Redux, Thunk, and Typescript</code>.
-        </p>
-      </header>
-      Results:
-      <div className="results">
-        {!loading &&
-          testTodos?.length &&
-          [...testTodos].map((testItem) => (
-            <div className="test" key={testItem.id}>
-              <p>{testItem.title}</p>
-            </div>
-          ))}
-      </div>
-    </div>
-  );
-}
+const App = () => {
+  return <div className="App">Hello wold!</div>;
+};
 
 export default App;
