fc927966b8974f35a8db9489d8ce4290210a6e0c
kevin nacario <hitchcliff1@gmail.com>
2020-11-17 07:11:32 +0800

    ecosystem setup

--- src/App.tsx
+++ src/App.tsx
@@ -6,28 +6,33 @@ import { TestActions } from './Actions/test.action';
 import { RootStore } from './Store';
 
 function App() {
-  const state = useSelector((state: RootStore) => state.test)
+  const state = useSelector((state: RootStore) => state.test);
   const { loading, testTodos } = state;
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(TestActions());
-  }, [])
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>This is a boilerplate for <code>React, Redux, Thunk, and Typescript</code>.</p>
+        <p>
+          This is a boilerplate for{' '}
+          <code>React, Redux, Thunk, and Typescript</code>.
+        </p>
       </header>
-        Results:
-        <div className="results">
-          {!loading && testTodos?.length && [...testTodos].map((testItem) => (
+      Results:
+      <div className="results">
+        {!loading &&
+          testTodos?.length &&
+          [...testTodos].map((testItem) => (
             <div className="test" key={testItem.id}>
-                <p>{testItem.title}</p>
+              <p>{testItem.title}</p>
             </div>
           ))}
-        </div>
+      </div>
     </div>
   );
 }
