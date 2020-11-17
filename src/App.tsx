33872e61c12849b193b5d0791592d3f91c60df75
kevin nacario <hitchcliff1@gmail.com>
2020-11-17 08:01:50 +0800

    did some refixing with node sass, seems bumped

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import './App.component.scss';
+
+// main components
 import { Overview, Sidebar, Table } from './components';
 
 const App = () => {
   return (
     <div className="App">
-      <Sidebar />
-      <Table />
-      <Overview />
+      <div className="container container--sidebar sidebar">
+        <Sidebar />
+      </div>
+      <div className="container container--main">
+        <Table />
+        <Overview />
+      </div>
     </div>
   );
 };
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import './index.component.scss';
 /**
  * Functional react component for congratulatory message.
  * @function
  * @returns {JSX.Element} - Rendered component (or null if `success` prop is missing)
  */
 const Sidebar = () => {
-  return <div>Hello from sidebar</div>;
+  return <aside className="sidebar">Hello from sidebar</aside>;
 };
 export default Sidebar;
