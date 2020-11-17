c4d59b4f9f9de54880b1a35f2b1468b81123f95c
kevin nacario <hitchcliff1@gmail.com>
2020-11-17 08:29:13 +0800

    init 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.component.scss';
+import './App.scss';
 
 // main components
 import { Overview, Sidebar, Table } from './components';
@@ -7,10 +7,10 @@ import { Overview, Sidebar, Table } from './components';
 const App = () => {
   return (
     <div className="App">
-      <div className="container container--sidebar sidebar">
+      <div className="col col--sidebar">
         <Sidebar />
       </div>
-      <div className="container container--main">
+      <div className="col col--overview">
         <Table />
         <Overview />
       </div>
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import './index.component.scss';
+import styles from './index.module.scss';
 /**
  * Functional react component for congratulatory message.
  * @function
  * @returns {JSX.Element} - Rendered component (or null if `success` prop is missing)
  */
 const Sidebar = () => {
-  return <aside className="sidebar">Hello from sidebar</aside>;
+  return <aside className={styles.sidebar}>Hello from sidebar</aside>;
 };
 export default Sidebar;
