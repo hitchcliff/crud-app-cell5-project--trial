a64daeea8fb214f648188379031de39ecc10377c
kevin nacario <hitchcliff1@gmail.com>
2020-11-17 22:01:48 +0800

    added delete in tables

--- src/components/TableRows/TableRows.tsx
+++ src/components/TableRows/TableRows.tsx
@@ -1,11 +1,13 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { Client } from '../../Actions/clients.action';
 import styles from './TableRows.module.scss';
+
 interface ITableRows {
   clients: Client[];
 }
 
 interface EditableTable {
+  id?: number;
   gender?: string;
   first_name?: string;
   last_name?: string;
@@ -22,16 +24,28 @@ interface EditableTable {
  */
 const TableRows = ({ clients }: ITableRows): JSX.Element => {
   const [currentIndex, setCurrentIndex] = useState<number | null>();
+  const [data, setData] = useState<Client[]>();
   const [state, set] = useState<EditableTable>({}); // new values temporarily stored
+
+  useEffect(() => {
+    const req = setTimeout(() => {
+      setData(clients);
+    });
+    return () => {
+      clearTimeout(req);
+    };
+  }, [clients]);
 
   const onSave = (id: number) => {
     setCurrentIndex(null); // reset
-    const test = clients.find((item) => {
+    const findItem = data?.find((item) => {
       return item.id === id;
     });
 
-    const newTest = { ...test, ...state };
-    console.log(newTest);
+    const found = { ...findItem, ...state }; // once it is found
+    const filter = data?.filter((item) => item.id !== found.id); // start filtering the data
+    const newData: any = [found, ...filter]; // spread the data
+    setData(newData);
   };
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -42,19 +56,36 @@ const TableRows = ({ clients }: ITableRows): JSX.Element => {
     set({ [name]: value });
   };
 
-  const onEdit = (id: number) => {
-    setCurrentIndex(id);
-    // if currentIndex === id, that means user is saving
-    if (id === currentIndex) {
-      onSave(id);
+  const onEdit = (id: number, type: string) => {
+    if (type === 'edit') {
+      setCurrentIndex(id);
+      // if currentIndex === id, that means user is saving
+      if (id === currentIndex) {
+        onSave(id);
+      }
+    } else if (type === 'delete') {
+      const deleteValue = data?.filter((item) => item.id !== id);
+      setData(deleteValue);
     }
   };
 
   // Simply map the clients array from the server
-  const mappingClients = clients.map((client: Client, index: number) => {
-    const { id, gender, first_name, last_name, mobile_number, bills } = client;
+  const mappingClients = data?.map((client: Client, index: number) => {
+    const {
+      id,
+      gender,
+      first_name,
+      last_name,
+      mobile_number,
+      bills,
+      paid,
+    } = client;
     return (
-      <tr key={index} className={styles.body}>
+      <tr
+        key={index}
+        className={styles.body}
+        data-color={`${paid ? 'completed' : 'not-completed'}`}
+      >
         <td>{index + 1}</td>
 
         {/* If any Edit button is clicked, we then compare the `id` and `currentIndex` which was
@@ -66,7 +97,7 @@ const TableRows = ({ clients }: ITableRows): JSX.Element => {
               name="gender"
               className={styles.body__input}
               placeholder={gender} // telling typescript to accept undefined values
-              onChange={(e) => onChange(e)} // this is where the logic are of adding new values in state
+              onChange={(e) => onChange(e)} // this is where the logic of adding new values in state
             />
           ) : (
             client.gender
@@ -74,16 +105,16 @@ const TableRows = ({ clients }: ITableRows): JSX.Element => {
         </td>
 
         <td>{client.first_name}</td>
-
         <td>{client.last_name}</td>
         <td>{client.mobile_number}</td>
         <td>${client.bills}</td>
 
         {/* delete, update, and toggle complete */}
         <td className={styles.body__actions}>
-          <button onClick={() => onEdit(id)}>
+          <button onClick={() => onEdit(id, 'edit')}>
             {currentIndex === id ? 'save' : 'edit'}
           </button>
+          <button onClick={() => onEdit(id, 'delete')}>delete</button>
         </td>
       </tr>
     );
